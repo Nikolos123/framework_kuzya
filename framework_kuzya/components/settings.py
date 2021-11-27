1941c6860020845f616e3b91fa388db53db6b444
Nikolos123 <Isponec2>
2021-11-27 11:24:59 +0300

    add set address in settings

--- framework_kuzya/components/settings.py
+++ framework_kuzya/components/settings.py
@@ -4,3 +4,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 STATIC_FILES_DIR = path.join(ROOT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+PORT = 8080
+URL_ADDRESS = 'http://127.0.0.1'
--- framework_kuzya/run.py
+++ framework_kuzya/run.py
@@ -1,4 +1,6 @@
 from wsgiref.simple_server import make_server
+
+from components.settings import URL_ADDRESS, PORT
 from framework_kuzya.main import Framework
 from views import routes
 from components import settings
@@ -6,6 +8,7 @@ from components import settings
 # Создаем объект WSGI-приложения
 application = Framework(settings, routes)
 
-with make_server('', 8085, application) as httpd:
-    print("Запуск на порту 8080...")
+with make_server('', PORT, application) as httpd:
+    print("Запуск сервера")
+    print(f'{URL_ADDRESS}:{PORT}')
     httpd.serve_forever()
